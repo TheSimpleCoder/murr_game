9476d05eded307dea42d488ce190f52b3bf33de8
Your Name <you@example.com>
2019-10-27 21:58:29 +0300

    add move and lookMouse script

--- Assets/Scenes/MouseLook.cs
+++ Assets/Scenes/MouseLook.cs
@@ -15,8 +15,8 @@ public class MouseLook : MonoBehaviour
     public float sensitivityHor = 9.0f;
     public float sensitivityVert = 9.0f;
 
-    public float minimumVert = -45.0f;
-    public float maximumVert = 45.0f;
+    public float minimumVert = -90.0f;
+    public float maximumVert = 90.0f;
 
     private float _rotationX;
 
