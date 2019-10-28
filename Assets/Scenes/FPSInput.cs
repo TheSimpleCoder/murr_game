a017cd603975b24d3cdf32e9cced4b812e094671
Egor <Egor.Komarov@acronis.com>
2019-10-28 16:19:10 +0300

    add gravity

--- Assets/Scenes/FPSInput.cs
+++ Assets/Scenes/FPSInput.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// скрипт будет работать только если на элементе висит CharacterController
+[RequireComponent(typeof(CharacterController))]
+// позволяет через кнопку Add Component в инспекторе навесить текущий скрипт на элемент
+[AddComponentMenu("Control Script/FPS Input!!!!!")]
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
+    public float gravity = -9.8f;
     private CharacterController _charController;
 
     void Start()
@@ -16,16 +21,15 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
+        // Time.deltaTime - фиксирует количество кадров на разных компьютерах
+        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
 
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
-
-
+        movement.y = gravity;
+        
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
         _charController.Move(movement);
-
-        // Time.deltaTime - фиксирует количество кадров на разных компьютерах
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
     }
 }
