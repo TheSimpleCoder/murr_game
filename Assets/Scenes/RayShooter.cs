0eb1d383d926a07416db39602f97e4669b5334d5
Your Name <you@example.com>
2019-10-29 04:55:38 +0300

    edd first enemy

--- Assets/Scenes/RayShooter.cs
+++ Assets/Scenes/RayShooter.cs
@@ -11,6 +11,18 @@ public class RayShooter : MonoBehaviour
     {
         // доступ к конкретному компоненту
         _camera = GetComponent<Camera>();
+
+        // скрываем указатель мыши на экране
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 
     void Update()
@@ -25,7 +37,20 @@ public class RayShooter : MonoBehaviour
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit)) {
-                StartCoroutine(SphereIndicator(hit.point));
+
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+
+                if (target != null)
+                {
+                    target.ReactToHit();
+                }
+
+                else
+
+                {
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
             }
         }
     }
